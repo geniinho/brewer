5f52b4a68972b8929e73959a24766c42cf08f386
geniinho@hotmail.com <18031993>
2022-05-25 11:53:57 -0300

    config e controllers 100% ok

---
+++ src/main/java/com/algaworks/brewer/controller/ProdutosController.java
@@ -0,0 +1,31 @@
+package com.algaworks.brewer.controller;
+
+import com.algaworks.brewer.model.Produto;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+public class ProdutosController {
+
+    @RequestMapping("/produtos/novo")
+    public String novo(Produto produto){
+        return "produto/CadastroProduto";
+    }
+
+    @RequestMapping(value = "/produtos/novo", method = RequestMethod.POST)
+    public String cadastrar(@Valid Produto produto, BindingResult result,Model model, RedirectAttributes attributes){
+        if (result.hasErrors()){
+            return novo(produto);
+        }
+        attributes.addFlashAttribute("mensagem","Produto salvo com sucesso.");
+        System.out.println(">>> sku" + produto.getSku());
+        return "redirect:/produtos/novo";
+    }
+}
