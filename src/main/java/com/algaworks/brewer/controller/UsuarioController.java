28256b7d507c2e1b9a7e468710ea981e159823fe
geniinho@hotmail.com <18031993>
2022-05-27 12:15:40 -0300

    Usuario.java + UsuarioController.java + CadastroUsuario.html

---
+++ src/main/java/com/algaworks/brewer/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.controller;
+
+import com.algaworks.brewer.model.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsuarioController {
+    @RequestMapping("/usuarios/novo")
+    public String novo(Usuario usuario){
+        return "usuario/CadastroUsuario";
+    }
+}
