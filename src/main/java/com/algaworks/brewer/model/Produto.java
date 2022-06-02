da608205cc67a61ccfc2d940366caf7baad6e14d
geniinho@hotmail.com <18031993>
2022-06-02 11:08:27 -0300

    validação de todos os campos da entidade produto

--- src/main/java/com/algaworks/brewer/model/Produto.java
+++ src/main/java/com/algaworks/brewer/model/Produto.java
@@ -3,9 +3,7 @@ package com.algaworks.brewer.model;
 import com.algaworks.brewer.validation.SKU;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -15,22 +13,44 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
+
     @SKU
     @NotBlank(message = "SKU é obrigatório")
     private String sku;
+
     @NotBlank(message = "Nome é obrigatório")
     private String nome;
+
     @Size(min = 1, max = 50, message = "O tamanho da descrição deve ser entre 1 e 50")
     private String descricao;
+
+    @NotNull(message = "Valor é obrigatório")
+    @DecimalMin(value = "0.50", message = "O valor da cerveja deve ser maior que R$0,50")
+    @DecimalMax(value = "9999999.99", message = "O valor da cerveja deve ser menor que R$9.999.999,99")
     private BigDecimal valor;
+
+    @NotNull(message = "O teor alcóolico é obrigatório")
+    @DecimalMax(value = "100.0", message = "O valor do teor alcóolico deve ser menor que 100")
     @Column(name = "teor_alcoolico")
     private BigDecimal teorAlcoolico;
+
+    @NotNull(message = "A comissão é obrigatória")
+    @DecimalMax(value = "100.0", message = "A comissão deve ser igual ou menor que 100")
     private BigDecimal comissao;
+
+    @NotNull(message = "A quantidade em estoque é obrigatória")
+    @Max(value = 9999, message = "A quantidade em estoque deve ser menor que 9.999")
     @Column(name = "quantidade_estoque")
     private Integer quantidadeEstoque;
+
+    @NotNull(message = "A origem é obrigatória")
     @Enumerated(EnumType.STRING)
     private Origem origem;
+
+    @NotNull(message = "O sabor é obrigatório")
     private Sabor sabor;
+
+    @NotNull(message = "O estilo é obrigatório")
     @ManyToOne
     @JoinColumn(name = "codigo_estilo")
     private Estilo estilo;
